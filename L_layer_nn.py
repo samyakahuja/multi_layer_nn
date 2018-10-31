2adae042b6763f2ceb7544b79cfaf2979a2a6e5d
kaymas <samyakahuja24@hotmail.com>
2018-10-31 13:55:33 +0530

    added data desc and modified parameter initialization

--- L_layer_nn.py
+++ L_layer_nn.py
@@ -25,7 +25,7 @@ def initialize_parameters_deep(layer_dims):
     L = len(layer_dims)            
 
     for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01 #np.sqrt(2 / layer_dims[l-1])
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2 / layer_dims[l-1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
         
         assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
